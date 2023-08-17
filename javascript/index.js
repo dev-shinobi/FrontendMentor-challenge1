bf12075aff8d0f606bd3f92e38df7e580059c41f
dev-shinobi <dennisonwuegbu@gmail.com>
2023-08-17 10:44:51 +0100

    frontend mentor newsletter challenge

---
+++ javascript/index.js
@@ -0,0 +1,23 @@
+document.getElementById('submit').addEventListener('click', submitSuccess);
+function submitSuccess(e) {
+  const email = document.getElementsByClassName('text-and-input__inputfield');
+    e.preventDefault();
+    const emailInput = email[0].value;
+    if( emailInput && emailInput.includes('@') && emailInput.includes('.com','.net','.edu','.biz','.xyz')){
+    document.getElementById("newsletter").classList.add("hide");
+    document.getElementById("successDiv").classList.add("showflex");
+    
+    }
+    else{
+      document.getElementById("inputError").classList.add("show");
+      document.getElementById("inputID").style.borderColor = "#D57A7B";
+      document.getElementById("inputID").style.color = "#D57A7B";
+    }
+    
+  }
+  document.getElementById('dismiss').addEventListener('click', Dismissfunc);
+  function Dismissfunc(e) {
+    document.getElementById("successDiv").style.display = "none";
+    document.getElementById("newsletter").classList.add("showgrid");
+  }
+  
